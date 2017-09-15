be49d8f02ec5a9ed7c7fa0f595643987b22c3bfe
Sebastian Baum <sebastian.baum@ion2s.com>
2017-09-15 10:01:20 +0200

    - added support for php artisan view:clear

---
+++ recipes/view_clear.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: laravel-artisan
+# Recipe:: view:clear
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+include_recipe 'laravel-artisan::default'
+
+COMMAND = 'view:clear'
+
+execute "php artisan #{COMMAND}" do
+  cwd node['laravel-artisan']['path']
+  command "#{node['laravel-artisan']['call']} #{COMMAND} #{node['laravel-artisan']['verbosity']}"
+  action :run
+end
