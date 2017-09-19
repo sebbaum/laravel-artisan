4294e0961e65ed46d730fde0646dd906b2b66be7
Sebastian Baum <sebastian.baum@ion2s.com>
2017-09-19 09:28:57 +0200

    - installing and preparing supervisor

---
+++ attributes/supervisor.rb
@@ -0,0 +1,2 @@
+default['laravel-artisan']['supervisor']['main_conf_dir'] = '/var/supervisor'
+default['laravel-artisan']['supervisor']['conf_dir'] = "#{node['laravel-artisan']['supervisor']['main_conf_dir']}/conf"
--- recipes/queue_work.rb
+++ recipes/queue_work.rb
@@ -4,37 +4,14 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-include_recipe 'laravel-artisan::default'
+#include_recipe 'laravel-artisan::default'
 
-## install pip first
-package 'python-pip' do
-  action :install
-end
+### Install and prepare supervisor
+include_recipe 'laravel-artisan::supervisor'
 
-#bash 'install something' do
-#  user 'root'
-#  cwd '/tmp'
-#  code <<-EOH
-#  
-#  EOH
-#end
-
-execute 'install supervisor' do
-  command 'pip install supervisor'
-  action :run
-end
 
 COMMAND = 'queue:work'
 
-#supervisord_program "php artisan #{COMMAND} as a supervisor service" do
-#	name 'laravel-queue'
-#	command "#{node['laravel-artisan']['path']} #{node['laravel-artisan']['call']} #{COMMAND} #{node['laravel-artisan']['verbosity']}"
-#	action [:supervise, :start]
-#end
-
-
-#execute "php artisan #{COMMAND}" do
-#  cwd node['laravel-artisan']['path']
-#  command "#{node['laravel-artisan']['call']} #{COMMAND} #{node['laravel-artisan']['verbosity']}"
-#  action :run
-#end
+# TODOs
+# - Add a supervisor config for php artisan queue:work
+# - Start supervisor
