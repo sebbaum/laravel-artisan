09c11679a949e035da5e68062ded8a6225590fcb
Sebastian Baum <seb.baum@googlemail.com>
2017-10-15 21:10:57 +0200

    - example custom resource call for artisan

--- recipes/testing.rb
+++ recipes/testing.rb
@@ -6,3 +6,13 @@ end
 test_res 'my resource 2' do
 	message 'run test'
 end
+
+# artisan 'down'
+
+# artisan 'example' do
+#	action :run # default :run
+#	check false # default: true
+#	command 'up' # == name
+#	path node['laravel-artisan']['path'] # required
+#	verbosity '-v' # default '-v'
+# end
