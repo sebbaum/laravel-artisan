e9280ab6e82c5663e1a7b14af16943d832eb7141
Sebastian Baum <sebastian.baum@ion2s.com>
2017-10-16 12:36:03 +0200

    - using custom resource for env

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,10 +4,6 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-# Run the php artisan command with the `help` argument in order to print the help
-# and check that the path to the executable is set properly.
-
-
 artisan 'list' do
 	path node['laravel-artisan']['path']
 end
